bd818d01ebf6a6a796d38b29a345b4600a699917
nyeinchan <aungNyeinChan.dev@gmail.com>
2024-10-29 12:47:01 +0630

    computed property

--- vue_cli/my-blog/src/App.vue
+++ vue_cli/my-blog/src/App.vue
@@ -1,18 +1,15 @@
 <template>
-    <section>
-
-        <router-view></router-view>
-        <TestRef/>
-        <ReactiveComponent/>
-
-    </section>
+  <section>
+    <router-view></router-view>
+    <TestRef />
+    <ReactiveComponent /><br />
+    <hr />
+    <ComputedComponent />
+  </section>
 </template>
 
-
-<script setup >
-import TestRef from './components/Test-Ref.vue';
-import ReactiveComponent from './components/ReactiveComponent.vue';
-
-
+<script setup>
+import TestRef from "./components/Test-Ref.vue";
+import ReactiveComponent from "./components/ReactiveComponent.vue";
+import ComputedComponent from "./components/ComputedComponent.vue";
 </script>
-
---
+++ vue_cli/my-blog/src/components/ComputedComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <div class="container min-vh-100 my-3">
+      <h1 class="p-2 bg-primary mx-4 text-white rounded text-center">
+        Computed
+      </h1>
+      <div class="col-6 offset-3">
+        <input type="text" class="form-control" v-model="search" />
+        <ul class="my-2">
+          <li v-for="customer in searchCustomers" :key="customer">
+            {{ customer }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup>
+console.log("setup");
+
+import { computed, onMounted, onUpdated, ref } from "vue";
+
+const customers = ref(["mgmg", "aungaung", "susu", "koko", "ayeaye"]);
+const search = ref("");
+
+const searchCustomers = computed(() => {
+  return customers.value.filter((customer) => {
+    return customer.includes(search.value);
+  });
+});
+
+onMounted(() => {
+  console.log("onmouted");
+});
+
+onUpdated(() => {
+  console.log("onUpdate");
+});
+</script>
